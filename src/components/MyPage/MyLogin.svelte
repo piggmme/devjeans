a15b7e280388aacde58112102ddc152306ca9a6f
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-30 12:12:02 +0900

    :ambulance: 모바일 환경 로그인 안되는 버그- 알림 공지

--- src/components/MyPage/MyLogin.svelte
+++ src/components/MyPage/MyLogin.svelte
@@ -2,12 +2,21 @@
   import Login from '../Login.svelte'
   import Noti from '../Noti.svelte'
 
+  $: isMobile = window.hasOwnProperty('ontouchstart')
+
   export let title = '마이페이지를 이용하려면 로그인하세요.'
 </script>
 
 <div class="container">
   <div class="wrapper">
     <h2>{title}</h2>
+
+    {#if isMobile}
+      <div class:isMobile>
+        <span>아직 모바일에서는 로그인이 안돼요 ㅠㅠ</span>
+        <span>데스크탑에서 로그인 해주세요!</span>
+      </div>
+    {/if}
 
     <Login />
   </div>
@@ -18,6 +27,15 @@
   .container {
     width: 100%;
     padding: 10px;
+  }
+  .isMobile {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 13px;
+    margin-bottom: 20px;
+    color: #ff595e;
   }
   .wrapper {
     width: 100%;
