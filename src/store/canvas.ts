d17b2c55973e2b1bf396a3bd4b8380c4bf306f9a
cherry.k <cherry.k@kakaoenterprise.com>
2023-04-17 22:54:15 +0900

    :bug: 캔버스크기 작아지는 버그

--- src/store/canvas.ts
+++ src/store/canvas.ts
@@ -427,7 +427,7 @@ export const addCostume = (costume: CostumeKeys) => {
     let costumeImg = currentCostume.src
 
     fabric.Image.fromURL(costumeImg, function (img) {
-      img.scaleToWidth($width)
+      img.scaleToWidth($width * $canvas.getZoom())
       img.selectable = false
 
       img.set('itemType', 'costume')
@@ -469,6 +469,8 @@ export const addCostume = (costume: CostumeKeys) => {
 
 const uploadColorableCostume = (costume: string, lineImg: HTMLImageElement, colorableImg: HTMLImageElement) => {
   const $canvas = get(canvas)
+  const $width = get(width)
+
   const isReady = get(isReadyCostume)
   if (!isReady[costume]) {
     isReadyCostume.update((prev) => ({...prev, [costume]: true}))
@@ -485,7 +487,7 @@ const uploadColorableCostume = (costume: string, lineImg: HTMLImageElement, colo
     itemType: 'costume',
     costume,
   })
-  group.scaleToWidth($canvas.width)
+  group.scaleToWidth($width * $canvas.getZoom())
   $canvas.add(group)
   $canvas.renderAll()
 
