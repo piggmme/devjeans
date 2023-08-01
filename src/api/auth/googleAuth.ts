e4e4cea3a8a53cd1d5bc589c0af455df1f2f92c5
cherry.k <cherry.k@kakaoenterprise.com>
2023-08-01 22:17:01 +0900

    :bug: 구글 로그인 버튼 수정

--- src/api/auth/googleAuth.ts
+++ src/api/auth/googleAuth.ts
@@ -3,10 +3,9 @@ import {login} from '../service/auth'
 
 type InitAuthProps = {
   ref: HTMLElement
-  text?: google.accounts.id.GsiButtonConfiguration['text']
 }
 
-export const initAuth2 = ({ref, text = 'signin_with'}: InitAuthProps) => {
+export const initAuth2 = ({ref}: InitAuthProps) => {
   google.accounts.id.initialize({
     client_id: import.meta.env.VITE_APP_GOOGLE_CLIENT_ID,
     callback: async (res) => {
@@ -17,6 +16,6 @@ export const initAuth2 = ({ref, text = 'signin_with'}: InitAuthProps) => {
 
   google.accounts.id.renderButton(
     ref,
-    {theme: 'outline', size: 'large', text, width: '200', type: 'standard'}, // customization attributes
+    {theme: 'outline', size: 'large', type: 'standard'}, // customization attributes
   )
 }
